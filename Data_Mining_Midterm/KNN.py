7c7300358019396fc85f3d9be29fb8eb80614fc4
Benny0w0Liu <bennyliu9005@gmail.com>
2025-03-27 15:28:52 +0800

    create git

---
+++ Data_Mining_Midterm/KNN.py
@@ -0,0 +1,39 @@
+import csv
+def KNN(K, test_data,training_datas):
+    #calculate distance
+    data_distances=[]
+    for data in training_datas:
+        distance=0
+        for key in data:
+            if(key!='Outcome'):
+                distance+=pow(float(data[key])-float(test_data[key]),2)
+        data_distances.append({'dis':pow(distance,0.5),'Outcome':data['Outcome']})
+    #get K closest data
+    data_distances = sorted(data_distances, key=lambda d: d['dis'])
+    Outcome=[0,0]
+    for data in data_distances[0:K]:
+        Outcome[int(data['Outcome'])]+=1
+    #choose the greater class
+    return int(Outcome[1]>Outcome[0])
+
+def experiment(experiment):
+    #read train data
+    training_datas=[]
+    with open("./實驗"+experiment+"/train_data.csv", mode ='r') as file:    
+        csvFile = csv.DictReader(file)
+        for lines in csvFile:
+            training_datas.append(lines)
+    #read test data
+    test_datas=[]
+    with open("./實驗"+experiment+"/test_data.csv", mode ='r') as file:    
+        csvFile = csv.DictReader(file)
+        for lines in csvFile:
+                test_datas.append(lines)
+    #get Accuracy
+    correct_answer=0
+    for data in test_datas:
+        correct_answer+=int(int(data['Outcome'])==KNN(5,data,training_datas))
+    print("Experiment",experiment,'Accuracy:',correct_answer*100/len(test_datas),'%')
+
+experiment("A")
+experiment("B")
